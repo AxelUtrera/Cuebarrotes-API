65289369d392d1e8aa73d276360da816a61f3fd9
Marlon Montiel Pérez <mrmarlonmp@gmail.com>
2023-12-10 18:41:25 -0600

    controlador de privilegios creado, encargado de verificar que el token JWT sea valido y esta siendo usando por el cliente autorizado

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,7 +6,15 @@ async function login(req, res) {
         const { numTelefono, contrasenia } = req.body;
         const user = await authenticateUser(numTelefono, contrasenia);
 
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const userRole = user.rol ? user.rol : "customer";
+
+        
+        const token = jwt.sign(
+            { userId: user._id, role: userRole },
+            process.env.JWT_SECRET,
+            { expiresIn: '1h' }
+        );
+
         res.json({ token });
 
     } catch (error) {
