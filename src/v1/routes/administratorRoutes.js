1164b44d1c305cffee4534a24a5dfead31b0928a
Marlon Montiel Pérez <mrmarlonmp@gmail.com>
2023-12-19 15:15:29 -0600

    Endpoint /createBranch, el cual crea la sucursal si cumple con las validaciones

--- src/v1/routes/administratorRoutes.js
+++ src/v1/routes/administratorRoutes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const { createProduct, addProductToBranch, createEmployee, getBranches } = require('../../controllers/administratorController');
+const { createProduct, addProductToBranch, createEmployee, getBranches, createBranch } = require('../../controllers/administratorController');
 const router = Router();
+const { verifyToken } = require('../../token/tokenValidation');
 
 router.post('/createEmployee', createEmployee);
 router.post('/createProduct', createProduct);
 router.patch('/addProductToBranch', addProductToBranch);
 router.get('/getBranchesInfo', getBranches);
+router.post('/createBranch', verifyToken(['Administrador']), createBranch);
 
 module.exports = router;
