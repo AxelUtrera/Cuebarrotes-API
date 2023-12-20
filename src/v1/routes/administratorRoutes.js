0dcf66be5c99e4322068616cb33507f7b88df0f8
Marlon Montiel Pérez <mrmarlonmp@gmail.com>
2023-12-19 23:08:16 -0600

    Agregue validaciones al endpoint de /createBranch

--- src/v1/routes/administratorRoutes.js
+++ src/v1/routes/administratorRoutes.js
@@ -2,11 +2,12 @@ const { Router } = require('express');
 const { createProduct, addProductToBranch, createEmployee, getBranches, createBranch } = require('../../controllers/administratorController');
 const router = Router();
 const { verifyToken } = require('../../token/tokenValidation');
+const { createBranchValidationRules, validateCreateBranch } = require('../../validations/branchValidation');
 
 router.post('/createEmployee', createEmployee);
 router.post('/createProduct', createProduct);
 router.patch('/addProductToBranch', addProductToBranch);
 router.get('/getBranchesInfo', getBranches);
-router.post('/createBranch', verifyToken(['Administrador']), createBranch);
+router.post('/createBranch', createBranchValidationRules(), validateCreateBranch,verifyToken(['Administrador']), createBranch);
 
 module.exports = router;
