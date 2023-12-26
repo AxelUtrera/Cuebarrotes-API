a1ec35ada4b47d5ac36067d1afae24c7f1c9e9cf
Marlon Montiel Pérez <mrmarlonmp@gmail.com>
2023-12-25 23:43:45 -0600

    Validacion de datos no repetidos

--- src/v1/routes/administratorRoutes.js
+++ src/v1/routes/administratorRoutes.js
@@ -2,12 +2,12 @@ const { Router } = require('express');
 const { createProduct, addProductToBranch, createEmployee, getBranches, createBranch } = require('../../controllers/administratorController');
 const router = Router();
 const { verifyToken } = require('../../token/tokenValidation');
-const { createBranchValidationRules, validateCreateBranch } = require('../../validations/branchValidation');
+const { createBranchValidationRules, validateCreateBranch, checkBranchExists } = require('../../validations/branchValidation');
 
 router.post('/createEmployee', createEmployee);
 router.post('/createProduct', createProduct);
 router.patch('/addProductToBranch', addProductToBranch);
 router.get('/getBranchesInfo', getBranches);
-router.post('/createBranch', createBranchValidationRules(), validateCreateBranch,verifyToken(['Administrador']), createBranch);
+router.post('/createBranch', createBranchValidationRules(), checkBranchExists,validateCreateBranch,verifyToken(['Administrador']), createBranch);
 
 module.exports = router;
--- src/validations/branchValidation.js
+++ src/validations/branchValidation.js
@@ -1,5 +1,6 @@
 // Tus imports existentes
 const { body, validationResult } = require('express-validator');
+const Branch = require('../models/branchModel');
 
 const createBranchValidationRules = () => {
     return [
@@ -19,4 +20,27 @@ const validateCreateBranch = (req, res, next) => {
     next();
 };
 
-module.exports = { createBranchValidationRules, validateCreateBranch };
+const checkBranchExists = async (req, res, next) => {
+    try {
+      const { nombreComercial, direccion, ubicacion } = req.body;
+  
+      
+      const existingBranch = await Branch.findOne({
+        $or: [
+          { nombreComercial: nombreComercial },
+          { direccion: direccion },
+          { 'ubicacion.latitud': ubicacion.latitud, 'ubicacion.longitud': ubicacion.longitud }
+        ]
+      });
+  
+      if (existingBranch) {
+        return res.status(400).json({ message: 'La sucursal con estos datos ya existe.' });
+      }
+  
+      next(); 
+    } catch (error) {
+      res.status(500).json({ message: 'Error al verificar la sucursal', error: error.message });
+    }
+  };
+
+module.exports = { createBranchValidationRules, validateCreateBranch, checkBranchExists };
